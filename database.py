3cafc95adf1850568222c40c98abea24164edbc2
IsaacIngram <me@isaacingram.dev>
2026-04-05 18:06:40 -0400

    Properly handle secret password

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from os import environ
+from urllib.parse import quote_plus
 
 DATABASE_URL = environ.get('DB_URL')
+
+import os
+
+password = quote_plus(os.getenv("DB_PASSWORD"))
+
+DATABASE_URL = f"postgresql+asyncpg://events_user:{password}@postgres:5432/events_db"
 
 engine = create_async_engine(DATABASE_URL)
 
